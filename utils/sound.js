faa4620286bbcfdf3a0360490cdd3cf4de04f59e
Cédric Syllebranque <cedric.syllebranque@administration.gov.pf>
2025-08-19 15:47:47 -1000

    Add some sound to round animation

--- utils/sound.js
+++ utils/sound.js
@@ -3,4 +3,10 @@ class Sound {
     var audio = new Audio(`../sound/hits/${zone}.mp3`);
     audio.play();
   }
+
+  static play(sound_url) {
+    console.log("Playing sound:", sound_url);
+    var audio = new Audio(`../sound/${sound_url}`);
+    audio.play();
+  }
 }
